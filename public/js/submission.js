3c2bff072d0660028042f9feaa97942ba8b99a9a
Nikola Lukic <nlukic97@gmail.com>
2021-10-12 01:47:04 +0200

    added some things to the js code

--- public/js/submission.js
+++ public/js/submission.js
@@ -1,28 +1,40 @@
 document.getElementById('email-submit').addEventListener('click',function(){
+        validate()
+})
+
+document.querySelector('input#email').addEventListener('keypress',function(e){
+    if(e.key === 'Enter') {
+        validate();
+    }
+})
+
+// --- functions ---
+
+function validate(){
     let email = document.getElementById('email').value
-    console.log(email);
+    if(email === ''){
+        return false
+    }
 
     let re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     let form = document.querySelector('.form')
 
     if(email.match(re) === null){
         showError(form)
-        console.log('not email');
     } else {
         hideError(form)
         submit(email)
         document.getElementById('email').value = ''
     }
-})
+}
 
 function submit(email){
     let data = {email:email}
     // submit this data somewhere
-    console.log('Submitting data',data);
+    console.log('Data to submit:',data);
 }
 
 function showError(item){
-    //display the background question mark, and the error text underneath
     item.classList.add('error')
 }
 
